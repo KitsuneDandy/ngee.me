6e7c41a6de889d894769f858184b8baf3f764ad2
Ngee <nicolas@ngee.me>
2019-05-03 20:09:05 +0900

    first save

---
+++ js/main.js
@@ -0,0 +1,11 @@
+window.addEventListener("load", (event) => { document.getElementById("body").classList.remove("preload"); })
+
+function openNav() {
+    document.getElementById("mobileNavBar").style.width = "100%";
+}
+
+function closeNav() {
+    document.getElementById("mobileNavBar").style.width = "0%";
+}
+
+
